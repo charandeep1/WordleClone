b87d16097e5138f3d8bf2df8ecb971db02838072
charan <satellyc@gmail.com>
2022-04-05 14:47:50 -0500

    Wrote till cell logic

--- App.js
+++ App.js
@@ -1,20 +1,75 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { borderLeftColor, color } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
+import { colors } from "./src/constants";
+import Keyboard from './src/components/Keyboard';
+import reactDom from 'react-dom';
 
 export default function App() {
+
+  const word = "whatsup";
+  const letters = word.split('');
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+
+      <StatusBar style="light" />
+
+      <Text style={styles.title}> WORDLE</Text>
+      <View style={styles.map}>
+        <View style={styles.row}>
+          {letters.map(letter => <View style={styles.cell} />)}
+
+
+
+
+
+
+        </View>
+      </View>
+      <Keyboard />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: colors.black,
     alignItems: 'center',
-    justifyContent: 'center',
+
+
   },
+  title: {
+    color: colors.lightgrey,
+    fontSize: 32,
+    fontWeight: 'bold',
+    letterSpacing: 7,
+
+  },
+  map: {
+
+    alignSelf: "stretch",
+    height: 100,
+
+  },
+  row: {
+
+    flexDirection: "row",
+    alignSelf: "stretch",
+    justifyContent: "center",
+
+  },
+  cell: {
+    borderWidth: 3,
+    borderColor: "darkgrey",
+    aspectRatio: 1,
+    flex: 1,
+    margin: 3,
+    maxWidth: 70,
+
+  },
+
+
 });
